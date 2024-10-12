32c7dfb400132dc3006eeaf3a8b533f1c2a3e34f
Ekr-125 <127433955+Ekr-125@users.noreply.github.com>
2024-10-12 12:25:19 -0600

    innitial commit

---
+++ styles/index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('La mas linda y bonita que mis ojos y mente a podido presenciar')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})  
